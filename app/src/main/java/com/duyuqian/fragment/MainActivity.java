8eda7d8b077488afa023d649bfd8dd62a3175a47
duyuqian <809009017@qq.com>
2020-08-14 12:08:30 +0800

    [duyuqian] fix: repalce different fragment when click different button

--- app/src/main/java/com/duyuqian/fragment/MainActivity.java
+++ app/src/main/java/com/duyuqian/fragment/MainActivity.java
@@ -1,8 +1,11 @@
 package com.duyuqian.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.android_btn).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        replaceFragment(new RightFragmentOfAndroid());
+                    }
+                }
+        );
+        findViewById(R.id.java_btn).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        replaceFragment(new RightFragmentOfJava());
+                    }
+                }
+        );
+    }
+
+
+    private void  replaceFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.right_layout,fragment);
+        transaction.commit();
     }
 }
