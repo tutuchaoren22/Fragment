5ea67a53004ff32ff9d7b7bc235177829d2d17c3
duyuqian <809009017@qq.com>
2020-08-14 20:29:48 +0800

    [duyuqian] refactor: refactor the MainActivity use ButterKnife

--- app/src/main/java/com/duyuqian/fragment/MainActivity.java
+++ app/src/main/java/com/duyuqian/fragment/MainActivity.java
@@ -6,35 +6,42 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.android_btn)
+    Button skipToAndroid;
+    @BindView(R.id.java_btn)
+    Button skipToJava;
+    @BindView(R.id.right_layout)
+    FrameLayout rightLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.android_btn).setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        replaceFragment(new RightFragmentOfAndroid());
-                    }
-                }
-        );
-        findViewById(R.id.java_btn).setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        replaceFragment(new RightFragmentOfJava());
-                    }
-                }
-        );
+        ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.android_btn)
+    public void onClickAndroidBtn() {
+        replaceFragment(new RightFragmentOfAndroid());
+    }
 
-    private void  replaceFragment(Fragment fragment){
+    @OnClick(R.id.java_btn)
+    public void onClickJavaBtn() {
+        replaceFragment(new RightFragmentOfJava());
+    }
+
+    private void replaceFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.right_layout,fragment);
+        transaction.replace(R.id.right_layout, fragment);
         transaction.commit();
     }
 }
